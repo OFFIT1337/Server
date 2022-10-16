8dae61abefddde06b2a29bf5f02c5d042bfdddcb
zaid <zaid97abyi@gmail.com>
2022-10-16 13:15:39 +0300

    Добавление кнопки "Подключиться"

--- TcpServer/ChatClient/Form1.Designer.cs
+++ TcpServer/ChatClient/Form1.Designer.cs
@@ -33,19 +33,22 @@
             this.richTextBoxMessage = new System.Windows.Forms.RichTextBox();
             this.buttonSend = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBoxIPServer = new System.Windows.Forms.RichTextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBoxChat
             // 
-            this.richTextBoxChat.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxChat.Location = new System.Drawing.Point(12, 56);
             this.richTextBoxChat.Name = "richTextBoxChat";
             this.richTextBoxChat.ReadOnly = true;
-            this.richTextBoxChat.Size = new System.Drawing.Size(628, 307);
+            this.richTextBoxChat.Size = new System.Drawing.Size(628, 280);
             this.richTextBoxChat.TabIndex = 0;
             this.richTextBoxChat.Text = "";
             // 
             // richTextBoxMessage
             // 
+            this.richTextBoxMessage.Enabled = false;
             this.richTextBoxMessage.Location = new System.Drawing.Point(12, 342);
             this.richTextBoxMessage.Name = "richTextBoxMessage";
             this.richTextBoxMessage.Size = new System.Drawing.Size(480, 96);
@@ -67,17 +70,37 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // richTextBoxIPServer
+            // 
+            this.richTextBoxIPServer.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxIPServer.Name = "richTextBoxIPServer";
+            this.richTextBoxIPServer.Size = new System.Drawing.Size(480, 38);
+            this.richTextBoxIPServer.TabIndex = 3;
+            this.richTextBoxIPServer.Text = "";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConnect.Location = new System.Drawing.Point(498, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(142, 38);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Подключиться";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(652, 450);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.richTextBoxIPServer);
             this.Controls.Add(this.buttonSend);
             this.Controls.Add(this.richTextBoxMessage);
             this.Controls.Add(this.richTextBoxChat);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -88,6 +111,8 @@
         private System.Windows.Forms.RichTextBox richTextBoxMessage;
         private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBoxIPServer;
+        private System.Windows.Forms.Button buttonConnect;
     }
 }
 
